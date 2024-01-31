0fd15cb8e64a79145eae42b3c4399ca062b41a80
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-01-31 11:06:50 -0300

    add main application

---
+++ src/main/java/com/acme/algafood/jpa/ConsultaCozinhaMain.java
@@ -0,0 +1,34 @@
+package com.acme.algafood.jpa;
+
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.acme.algafood.domain.model.Cozinha;
+
+@SpringBootApplication
+public class ConsultaCozinhaMain {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCozinhaMain.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+			CadastroCozinha cadastroCozinha = ctx.getBean(CadastroCozinha.class);
+
+			List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+			for (Cozinha cozinha : cozinhas) {
+				System.out.println(cozinha.getNome());
+			}
+		};
+	}
+
+}
