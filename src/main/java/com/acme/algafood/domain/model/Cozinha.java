f0756e7f127233ea5d8a5c446c84ae6401b77b73
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-01-31 22:36:06 -0300

    add lombok

--- src/main/java/com/acme/algafood/domain/model/Cozinha.java
+++ src/main/java/com/acme/algafood/domain/model/Cozinha.java
@@ -1,55 +1,25 @@
 package com.acme.algafood.domain.model;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 	
 	@Id
+	@EqualsAndHashCode.Include
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(length = 30)
 	private String nome;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, nome);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cozinha other = (Cozinha) obj;
-		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
-	}
-	
 	
 }
