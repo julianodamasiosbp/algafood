d030342fd59b03c1eaa1fa3006da16ba6ea626ab
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-02-09 16:56:21 -0300

    add update method cidade

--- src/main/java/com/acme/algafood/api/controller/CidadeController.java
+++ src/main/java/com/acme/algafood/api/controller/CidadeController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,16 @@ public class CidadeController {
 			return ResponseEntity.ok(cidade); 
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Cidade cidade){
+		try {
+			cidade = this.cidadeService.atualizar(id, cidade);
+			return ResponseEntity.ok(cidade); 			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
 		}
 	}
 
--- src/main/java/com/acme/algafood/domain/service/CidadeService.java
+++ src/main/java/com/acme/algafood/domain/service/CidadeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,18 +16,28 @@ import com.acme.algafood.domain.repository.CidadeRepository;
 public class CidadeService {
 
 	@Autowired
-	private CidadeRepository cidadeRepository; 
-	
-	public List<Cidade> listar(){
-		return this.cidadeRepository.listar(); 
+	private CidadeRepository cidadeRepository;
+
+	public List<Cidade> listar() {
+		return this.cidadeRepository.listar();
 	}
-	
+
 	public Cidade salvar(Cidade cidade) {
 		try {
 			Cidade cidadeSalva = this.cidadeRepository.salvar(cidade);
 			return cidadeSalva;
 		} catch (EntityNotFoundException e) {
-			throw new EntidadeNaoEncontradaException(String.format("Nao existe cadastro de Estado com codigo: %d", cidade.getEstado().getId()));
+			throw new EntidadeNaoEncontradaException(
+					String.format("Nao existe cadastro de Estado com codigo: %d", cidade.getEstado().getId()));
 		}
+	}
+
+	public Cidade atualizar(Long id, Cidade cidade) {
+		Cidade cidadeSalva = this.cidadeRepository.buscar(id);
+		if (cidadeSalva == null) {
+			throw new EntidadeNaoEncontradaException(String.format("Nao existe cadastro de Cidade com codigo: %d", id));
+		}
+		BeanUtils.copyProperties(cidade, cidadeSalva, "id");
+		return this.cidadeRepository.salvar(cidadeSalva);
 	}
 }
