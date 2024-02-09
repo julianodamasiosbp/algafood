74d1894fdb0a43fc12ca393e035e1d6ef19dd4b0
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-02-09 20:25:09 -0300

    add save method Estado

--- src/main/java/com/acme/algafood/api/controller/EstadoController.java
+++ src/main/java/com/acme/algafood/api/controller/EstadoController.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.acme.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.acme.algafood.domain.model.Estado;
 import com.acme.algafood.domain.repository.EstadoRepository;
+import com.acme.algafood.domain.service.EstadoService;
 
 @RequestMapping("/estados")
 @RestController
@@ -18,9 +24,28 @@ public class EstadoController {
 	@Autowired
 	private EstadoRepository estadoRepository;
 	
+	@Autowired
+	private EstadoService estadoService;
+	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Estado> listar() {
 		return estadoRepository.listar();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscar(@PathVariable Long id){
+		try {
+			Estado estado = this.estadoService.buscar(id);
+			return ResponseEntity.ok(estado);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> salvar(@RequestBody Estado estado){
+		estado = this.estadoService.salvar(estado);
+		return ResponseEntity.ok(estado);
 	}
 	
 }
---
+++ src/main/java/com/acme/algafood/domain/service/EstadoService.java
@@ -0,0 +1,29 @@
+package com.acme.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.acme.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.acme.algafood.domain.model.Estado;
+import com.acme.algafood.domain.repository.EstadoRepository;
+
+@Service
+public class EstadoService {
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	public Estado salvar(Estado estado) {
+		return this.estadoRepository.salvar(estado);
+	}
+	
+	public Estado buscar(Long id) {
+		Estado estadoSalvo = this.estadoRepository.buscar(id);
+		if(estadoSalvo == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Nao existe cadastro de Estado com codigo: %d", id));
+		}
+		return estadoSalvo;
+	}
+
+}
