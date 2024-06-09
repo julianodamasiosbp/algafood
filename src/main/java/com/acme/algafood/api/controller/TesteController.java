ec3c6abbd2ee74f3e8b43a6bad748c4a9290625b
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-06-09 17:53:52 -0300

    add get cozinha by name using JPQL

---
+++ src/main/java/com/acme/algafood/api/controller/TesteController.java
@@ -0,0 +1,25 @@
+package com.acme.algafood.api.controller;
+
+import com.acme.algafood.domain.model.Cozinha;
+import com.acme.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(@RequestParam String nome){
+        System.out.println(nome);
+        return cozinhaRepository.consultarPorNome(nome);
+    }
+}
--- src/main/java/com/acme/algafood/infrastructure/repository/EstadoRepositoryImpl.java
+++ src/main/java/com/acme/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -6,13 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.acme.algafood.domain.model.Estado;
 import com.acme.algafood.domain.repository.EstadoRepository;
 
-@Component
+@Repository
 public class EstadoRepositoryImpl implements EstadoRepository {
 
 	@PersistenceContext
