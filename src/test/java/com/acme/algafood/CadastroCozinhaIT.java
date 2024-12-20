20143e4bde69d7c5ef58c64e1a440253ed9a1dac
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-12-20 14:25:09 -0300

    add RestAssured dependency and tests

--- src/test/java/com/acme/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/acme/algafood/CadastroCozinhaIT.java
@@ -1,23 +1,41 @@
 package com.acme.algafood;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.validation.ConstraintViolationException;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
 import com.acme.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.acme.algafood.domain.exception.EntidadeEmUsoException;
 import com.acme.algafood.domain.model.Cozinha;
 import com.acme.algafood.domain.service.CozinhaService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.validation.ConstraintViolationException;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIT {
 
     @Autowired
     private CozinhaService cozinhaService;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
 
     @Test
 	public void deveAtribuirId_QuandoCozinhaCadastradaComDadosCorretos() {
@@ -60,5 +78,38 @@ class CadastroCozinhaIT {
                 });
 
         assertThat(erroEsperado).isNotNull();
+    }
+
+    @Test
+    public void deveRetornarStatus200_QuandoConsultarCozinhas(){
+        RestAssured.given()
+        .accept(ContentType.JSON)
+        .when()
+        .get()
+        .then()
+        .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas(){
+        RestAssured.given()
+        .accept(ContentType.JSON)
+        .when()
+        .get()
+        .then()
+        .body("", Matchers.hasSize(4))
+        .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+    }
+
+    @Test
+    public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+        RestAssured
+        .given()
+        .body("{ \"nome\": \"Chinesa\" }")
+        .contentType(ContentType.JSON)
+        .when()
+        .post()
+        .then()
+        .statusCode(HttpStatus.CREATED.value());
     }
 }
