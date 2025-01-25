7bc6aa71c0e33b07bde286f033e453089484098b
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-01-25 17:12:52 -0300

    adiciona restaurante assembler

--- src/main/java/com/acme/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/acme/algafood/api/controller/RestauranteController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.acme.algafood.api.assembler.RestauranteModelAssembler;
 import com.acme.algafood.api.model.CozinhaModel;
 import com.acme.algafood.api.model.RestauranteModel;
 import com.acme.algafood.api.model.input.CozinhaIdInput;
@@ -55,16 +56,19 @@ public class RestauranteController {
     @Autowired
     private SmartValidator validator;
 
+    @Autowired
+    private RestauranteModelAssembler restauranteModelAssembler;
+
     @GetMapping
     public List<RestauranteModel> listar() {
-        return toCollectionModel(restauranteRepository.findAll());
+        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
     }
 
     @GetMapping("/{restauranteId}")
     public RestauranteModel buscar(@PathVariable Long restauranteId) {
         Restaurante restaurante = restauranteService.buscarOuFalhar(restauranteId);
 
-        return toModel(restaurante);
+        return restauranteModelAssembler.toModel(restaurante);
     }
 
     @PostMapping
@@ -72,7 +76,7 @@ public class RestauranteController {
     public RestauranteModel adicionar(@RequestBody @Valid RestauranteInput restauranteInput) {
         try {
             Restaurante restaurante = toDomainObject(restauranteInput);
-            return toModel(restauranteService.salvar(restaurante));
+            return restauranteModelAssembler.toModel(restauranteService.salvar(restaurante));
         } catch (CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
         }
@@ -89,86 +93,10 @@ public class RestauranteController {
                 "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 
         try {
-            return toModel(restauranteService.salvar(restauranteAtual));
+            return restauranteModelAssembler.toModel(restauranteService.salvar(restauranteAtual));
         } catch (CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
         }
-    }
-
-    // @PatchMapping("/{restauranteId}")
-    // public RestauranteModel atualizarParcial(@PathVariable Long restauranteId,
-    // @RequestBody Map<String, Object> campos, HttpServletRequest request) {
-    // Restaurante restauranteAtual =
-    // restauranteService.buscarOuFalhar(restauranteId);
-
-    // merge(campos, restauranteAtual, request);
-    // validate(restauranteAtual, "restaurante");
-
-    // return atualizar(restauranteId, restauranteAtual);
-    // }
-
-    // private void validate(Restaurante restaurante, String objectName) {
-    // BeanPropertyBindingResult bindingResult = new
-    // BeanPropertyBindingResult(restaurante, objectName);
-    // validator.validate(restaurante, bindingResult);
-    // if (bindingResult.hasErrors()) {
-    // throw new ValidacaoException(bindingResult);
-    // }
-    // }
-
-    // private void merge(Map<String, Object> dadosOrigem, Restaurante
-    // restauranteDestino,
-    // HttpServletRequest request) {
-    // ServletServerHttpRequest serverHttpRequest = new
-    // ServletServerHttpRequest(request);
-
-    // try {
-    // ObjectMapper objectMapper = new ObjectMapper();
-    // objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES,
-    // true);
-    // objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-    // true);
-
-    // Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem,
-    // Restaurante.class);
-
-    // dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-    // Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-    // if (field != null) {
-    // field.setAccessible(true);
-
-    // Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-
-    // ReflectionUtils.setField(field, restauranteDestino, novoValor);
-    // }
-
-    // });
-    // } catch (IllegalArgumentException e) {
-    // Throwable rootCause = ExceptionUtils.getRootCause(e);
-    // throw new HttpMessageNotReadableException(e.getMessage(), rootCause,
-    // serverHttpRequest);
-    // }
-    // }
-
-    private RestauranteModel toModel(Restaurante restaurante) {
-        CozinhaModel cozinhaModel = new CozinhaModel();
-
-        cozinhaModel.setId(restaurante.getCozinha().getId());
-        cozinhaModel.setNome(restaurante.getCozinha().getNome());
-
-        RestauranteModel restauranteModel = new RestauranteModel();
-        restauranteModel.setId(restaurante.getId());
-        restauranteModel.setNome(restaurante.getNome());
-        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
-        restauranteModel.setCozinha(cozinhaModel);
-        return restauranteModel;
-    }
-
-    private List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
-        return restaurantes
-                .stream()
-                .map(restaurante -> toModel(restaurante))
-                .collect(Collectors.toList());
     }
 
     private Restaurante toDomainObject(RestauranteInput restauranteInput) {
