b422413772890cf9eeb55e53dfb1fa7721e665de
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-02-06 14:41:17 -0300

    feature: cria endpoint para remocao do usuario

--- src/main/java/com/acme/algafood/api/controller/UsuarioController.java
+++ src/main/java/com/acme/algafood/api/controller/UsuarioController.java
@@ -3,11 +3,14 @@ package com.acme.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acme.algafood.api.assembler.UsuarioInputDisassembler;
@@ -45,10 +48,17 @@ public class UsuarioController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public UsuarioModel adicionar(@RequestBody UsuarioInput usuarioInput) {
         Usuario novoUsuario = usuarioInputDisassembler.toDomainObject(usuarioInput);
         Usuario usuarioSalvo = usuarioService.salvar(novoUsuario);
         return usuarioModelAssembler.toModel(usuarioSalvo);
+    }
+
+    @DeleteMapping("/{usuarioId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long usuarioId) {
+        usuarioService.excluir(usuarioId);
     }
 
 }
--- src/main/java/com/acme/algafood/domain/service/UsuarioService.java
+++ src/main/java/com/acme/algafood/domain/service/UsuarioService.java
@@ -1,8 +1,13 @@
 package com.acme.algafood.domain.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.acme.algafood.domain.exception.EntidadeEmUsoException;
 import com.acme.algafood.domain.exception.UsuarioNaoEncontradoException;
 import com.acme.algafood.domain.model.Usuario;
 import com.acme.algafood.domain.repository.UsuarioRepository;
@@ -13,6 +18,8 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    private static final String MSG_USUARIO_EM_USO = "Usuário de código %d não pode ser removido, pois está em uso";
+
     public Usuario buscarOuFalhar(Long usuarioId) {
         return usuarioRepository.findById(usuarioId)
                 .orElseThrow(() -> new UsuarioNaoEncontradoException(usuarioId));
@@ -20,5 +27,21 @@ public class UsuarioService {
 
     public Usuario salvar(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    @Transactional
+    public void excluir(Long usuarioId) {
+        try {
+            usuarioRepository.deleteById(usuarioId);
+            usuarioRepository.flush();
+
+        } catch (EmptyResultDataAccessException e) {
+            throw new UsuarioNaoEncontradoException(
+                    usuarioId);
+
+        } catch (DataIntegrityViolationException e) {
+            throw new EntidadeEmUsoException(
+                    String.format(MSG_USUARIO_EM_USO, usuarioId));
+        }
     }
 }
