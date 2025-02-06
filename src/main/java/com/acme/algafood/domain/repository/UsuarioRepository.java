ca5f55fc0026622f3eeaa13a9e3403ef6234acbc
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-02-06 10:53:23 -0300

    feature: adiciona

---
+++ src/main/java/com/acme/algafood/domain/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.acme.algafood.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.acme.algafood.domain.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
