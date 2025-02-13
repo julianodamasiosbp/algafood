5187858a77ac5cdca135836e76adfcbebfb4494a
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-02-13 15:52:24 -0300

    feature: adiciona enpoint para listar permissoes do grupo

--- src/main/java/com/acme/algafood/api/controller/GrupoPermissaoController.java
+++ src/main/java/com/acme/algafood/api/controller/GrupoPermissaoController.java
@@ -1,16 +1,32 @@
 package com.acme.algafood.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.acme.algafood.api.assembler.PermissaoModelAssembler;
+import com.acme.algafood.api.model.response.PermissaoModel;
+import com.acme.algafood.domain.model.Grupo;
+import com.acme.algafood.domain.service.GrupoService;
 
 @RestController
 @RequestMapping("/grupos/{grupoId}")
 public class GrupoPermissaoController {
 
-    @GetMapping("/permissao")
-    public String hello() {
-        return "Hello World!";
+    @Autowired
+    private GrupoService grupoService;
+
+    @Autowired
+    private PermissaoModelAssembler permissaoModelAssembler;
+
+    @GetMapping("/permissoes")
+    public List<PermissaoModel> listar(@PathVariable Long grupoId) {
+        Grupo grupo = grupoService.buscarOuFalhar(grupoId);
+        return permissaoModelAssembler.toCollectionModel(grupo.getPermissoes());
     }
 
 }
