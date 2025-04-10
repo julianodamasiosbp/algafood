e79fc51592039c085123fd4faf4c18659bfd809d
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-04-10 16:50:59 -0300

    adiciona `vendaDiariaFilter`

--- src/main/java/com/acme/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/acme/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,13 +1,16 @@
 package com.acme.algafood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.stereotype.Repository;
 
+import com.acme.algafood.domain.enums.StatusPedido;
 import com.acme.algafood.domain.filter.VendaDiariaFilter;
 import com.acme.algafood.domain.model.Pedido;
 import com.acme.algafood.domain.model.dto.VendaDiaria;
@@ -21,9 +24,28 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 
     @Override
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
+        var predicates = new ArrayList<Predicate>();
+
         var builder = em.getCriteriaBuilder();
+
         var query = builder.createQuery(VendaDiaria.class);
+
         var root = query.from(Pedido.class);
+
+        if (filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+        }
+
+        if (filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
+        }
+
+        if (filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
+        }
+
+        predicates.add(root.get("status").in(
+                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
 
         var functionDateDataCriacao = builder
                 .function("date", Date.class, root.get("dataCriacao"));
@@ -33,6 +55,8 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                         builder.count(root.get("id")), builder.sum(root.get("valorTotal")));
 
         query.select(selection);
+
+        query.where(predicates.toArray(new Predicate[0]));
 
         query.groupBy(functionDateDataCriacao);
 
