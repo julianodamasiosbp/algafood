0485bff7707ae70b0f950f9f28bfc17edb0ba503
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-05-06 17:26:18 -0300

    add `RestauranteUsuarioResponsavelController` open api docs

--- src/main/java/com/acme/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/acme/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import com.acme.algafood.domain.model.Restaurante;
 import com.acme.algafood.domain.service.RestauranteService;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteId}/responsaveis")
+@RequestMapping(path = "/restaurantes/{restauranteId}/responsaveis", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestauranteUsuarioResponsavelController {
 
     @Autowired
