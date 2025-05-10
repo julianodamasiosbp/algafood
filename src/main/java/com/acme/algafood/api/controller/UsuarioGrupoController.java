b79f62a9e79e09691983a9321570ae1c38ba77f6
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-05-10 13:51:38 -0300

    add `UsuarioGrupoController` open api docs

--- src/main/java/com/acme/algafood/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/acme/algafood/api/controller/UsuarioGrupoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.acme.algafood.api.assembler.GrupoModelAssembler;
 import com.acme.algafood.api.model.response.GrupoModel;
+import com.acme.algafood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.acme.algafood.domain.model.Usuario;
 import com.acme.algafood.domain.service.UsuarioService;
 
 @RestController
-@RequestMapping("/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = "/usuarios/{usuarioId}/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     public GrupoModelAssembler grupoModelAssembler;
---
+++ src/main/java/com/acme/algafood/api/openapi/controller/UsuarioGrupoControllerOpenApi.java
@@ -0,0 +1,45 @@
+package com.acme.algafood.api.openapi.controller;
+
+import java.util.List;
+
+import com.acme.algafood.api.exceptionhandler.Problem;
+import com.acme.algafood.api.model.response.GrupoModel;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Usuários")
+public interface UsuarioGrupoControllerOpenApi {
+
+    @Operation(summary = "Lista os grupos associados a um usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = @Content(schema = @Schema(implementation = Problem.class)))
+    })
+    List<GrupoModel> listar(
+            @Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId);
+
+    @Operation(summary = "Desassociação de grupo com usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Desassociação realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário ou grupo não encontrado", content = @Content(schema = @Schema(implementation = Problem.class)))
+    })
+    void desassociar(
+            @Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId,
+
+            @Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId);
+
+    @Operation(summary = "Associação de grupo com usuário")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Associação realizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário ou grupo não encontrado", content = @Content(schema = @Schema(implementation = Problem.class)))
+    })
+    void associar(
+            @Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId,
+            @Parameter(description = "ID do grupo", example = "1", required = true) Long grupoId);
+
+}
