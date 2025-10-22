0356468baa5901d0accfd0530fe4d2842e0990c8
Juliano Damasio <julianodamasiosbp@gmail.com>
2025-10-22 14:35:25 -0300

    adiciona links hateoas para ativacao, inativacao, fechamento e abertura de restaurantes

--- src/main/java/com/acme/algafood/domain/model/Restaurante.java
+++ src/main/java/com/acme/algafood/domain/model/Restaurante.java
@@ -90,6 +90,38 @@ public class Restaurante {
                 setAberto(false);
         }
 
+        public boolean isAberto() {
+                return this.aberto;
+        }
+
+        public boolean isFechado() {
+                return !isAberto();
+        }
+
+        public boolean isInativo() {
+                return !isAtivo();
+        }
+
+        public boolean isAtivo() {
+                return this.ativo;
+        }
+
+        public boolean aberturaPermitida() {
+                return isAtivo() && isFechado();
+        }
+
+        public boolean ativacaoPermitida() {
+                return isInativo();
+        }
+
+        public boolean inativacaoPermitida() {
+                return isAtivo();
+        }
+
+        public boolean fechamentoPermitido() {
+                return isAberto();
+        }
+
         public boolean vincularFormaPagamento(FormaPagamento formaPagamento) {
                 return getFormasPagamento().add(formaPagamento);
         }
@@ -108,10 +140,10 @@ public class Restaurante {
 
         public boolean aceitaFormaPagamento(FormaPagamento formaPagamento) {
                 return getFormasPagamento().contains(formaPagamento);
-            }
-            
-            public boolean naoAceitaFormaPagamento(FormaPagamento formaPagamento) {
+        }
+
+        public boolean naoAceitaFormaPagamento(FormaPagamento formaPagamento) {
                 return !aceitaFormaPagamento(formaPagamento);
-            }
+        }
 
 }
